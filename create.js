d834a8c98002a010c39087c611fd8350db667a70
Erik <90757653+GeneralUser01@users.noreply.github.com>
2021-12-13 02:08:38 +0100

    Implemented error messages + name input on create which may be specified or omitted for a random name + made usage of configs on input instead of the created preview for generating worlds

--- create.js
+++ create.js
@@ -3,15 +3,19 @@ const buttons = {
     rectangle: document.getElementById('btn-rectangle'),
     branch: document.getElementById('btn-branch')
 };
+const display = document.getElementById('row-container');
+
 const optionalSections = document.querySelectorAll('[data-require-world]');
 const rememberWorldType = document.getElementById('remember-world-type');
 
 
+const nameConfig = document.getElementById('world-name');
 const lengthConfig = document.getElementById('world-side-length');
 const widthConfig = document.getElementById('world-side-width');
 const numberOfRoomsConfig = document.getElementById('world-number-of-rooms');
 const startLengthConfig = document.getElementById('world-start-length');
 const branchFactorConfig = document.getElementById('world-branch-factor');
+const branchCheckBoxConfig = document.getElementById("connect-branch-box");
 
 
 let wantedWorldType = 'circle';
@@ -36,47 +40,96 @@ onWorldTypeChanged();
 
 
 let currentWorld = null;
-let updateCurrentUi = function() {};
+let updateCurrentUi = function () { };
 const generateWorld = function (showAsWell = true) {
     let createdWorld;
-    if (isNaN(branchFactorConfig.value) || isNaN(numberOfRoomsConfig.value) || isNaN(lengthConfig.value) || isNaN(startLengthConfig.value) || isNaN(widthConfig.value)) {
-        console.log('NaNi!? Will not run invalid inputs');
-        return false;
-    }
-    const generate = document.getElementById("btn-generate");
-    if (wantedWorldType === 'circle' && numberOfRoomsConfig.value === 0 || wantedWorldType === 'rectangle' && lengthConfig.value === 0 || widthConfig.value === 0 || wantedWorldType === 'branch' && branchFactorConfig.value === 0 || startLengthConfig.value === 0) {
-        generate.disabled = true;
-        showWorld(null);
-        return false;
+    if (nameConfig.value === 'test message') {
+        console.log('message for display script triggered');
+        messageLabel = document.createElement('label');
+        messageLabel.innerHTML = 'Now this message is shown!';
+        document.getElementById('row-container').appendChild(messageLabel);
     }
 
-    if (wantedWorldType != 'circle' && numberOfRoomsConfig.value != 0 || wantedWorldType != 'rectangle' && lengthConfig.value != 0 || widthConfig.value != 0 || wantedWorldType != 'branch' && branchFactorConfig.value != 0 || startLengthConfig.value != 0) {
-        generate.disabled = false;
+    // Default world name
+    let worldName;
+    if (nameConfig.value) {
+        worldName = nameConfig.value;
+    } else {
+        let room = new Room();
+        worldName = room.name;
     }
 
+    let errorMessage = null;
     switch (wantedWorldType) {
         case 'circle':
-            createdWorld = new World('','circle', parseInt(numberOfRoomsConfig.value),0);
+            const numberOfRooms = parseInt(numberOfRoomsConfig.value);
+            if (isNaN(numberOfRooms)) {
+                errorMessage = 'Number of rooms is not a number';
+            } else if (numberOfRooms <= 0) {
+                errorMessage = 'Must be at least one room for a circular room';
+            } else if (numberOfRooms > 1000) {
+                errorMessage = 'There is such a thing as too big';
+            } else {
+                createdWorld = new World(worldName, 'circle', numberOfRooms);
+            }
             break;
         case 'rectangle':
-            createdWorld = new World('','rectangle', parseInt(lengthConfig.value),parseInt(widthConfig.value));
+            const length = parseInt(lengthConfig.value);
+            const width = parseInt(widthConfig.value);
+            if (isNaN(length) || isNaN(width)) {
+                errorMessage = ('Invalid inputs, need to be numbers');
+            } else if (length <= 0 || width <= 0) {
+                errorMessage = ('Numbers too small, must be 1 or larger');
+            } else if (length * width > 10000) {
+                errorMessage = ('There is such a thing as too big');
+            } else {
+                createdWorld = new World(worldName, 'rectangle', length, width);
+            }
             break;
         case 'branch':
-            createdWorld = new World('','branch', parseInt(startLengthConfig.value),parseFloat(branchFactorConfig.value));
+            const startLength = parseInt(startLengthConfig.value);
+            const branchFactor = parseFloat(branchFactorConfig.value);
+            if (isNaN(startLength) || isNaN(branchFactor)) {
+                errorMessage = 'Invalid inputs, need to be numbers';
+            } else if (startLength <= 0) {
+                errorMessage = 'Numbers too small, start length must be 1 or larger';
+            } else if (branchFactor < 0) {
+                errorMessage = 'Numbers too small, branch factor can\'t be smaller than 0';
+            } else if (branchFactor >= 1) {
+                errorMessage = 'Branch factor must be smaller than 1';
+            } else if (startLength > 1000) {
+                errorMessage = 'There is such a thing as too big';
+            } else {
+                createdWorld = new World(worldName, 'rectangle', length, width);
+            }
+            createdWorld = new World(worldName, branchCheckBoxConfig.checked ? 'branch-alternative' : 'branch', parseInt(startLengthConfig.value), parseFloat(branchFactorConfig.value));
             break;
     }
+
     if (createdWorld !== null) {
         if (showAsWell) {
             updateCurrentUi = showWorld(createdWorld);
         } else {
             showWorld(null);
-            updateCurrentUi = function() {};
+            updateCurrentUi = function () { };
         }
+        worldChangedSincePreview = false;
         currentWorld = createdWorld;
-        return true;
     } else {
-        return false;
+        showWorld(null);
+        updateCurrentUi = function () { };
     }
+
+    // Show error after we clear the world preview (since that clears eveything in the `row-container`)
+    if (errorMessage !== null) {
+        console.error('Failed to create world because: ', errorMessage);
+        messageLabel = document.createElement('span');
+        messageLabel.textContent = errorMessage;
+        display.appendChild(messageLabel);
+    }
+
+    // Return true if we successfully created a world:
+    return createdWorld !== null;
 };
 
 document.getElementById("preview-btn").addEventListener('click', generateWorld);
@@ -110,15 +163,26 @@ for (const [key, value] of Object.entries(buttons)) {
         // queueUpdate();
     };
 }
+
 // Updates preview as input is changed, not used due to design choice for only one button to queue updates
-// numberOfRoomsConfig.oninput = queueUpdate;
-// lengthConfig.oninput = queueUpdate;
-// widthConfig.oninput = queueUpdate;
-// startLengthConfig.oninput = queueUpdate;
-// branchFactorConfig.oninput = queueUpdate;
+let worldChangedSincePreview = true;
+function inputChanged() {
+    worldChangedSincePreview = true;
+}
+nameConfig.oninput = inputChanged;
+
+numberOfRoomsConfig.oninput = inputChanged;
+
+lengthConfig.oninput = inputChanged;
+widthConfig.oninput = inputChanged;
+
+startLengthConfig.oninput = inputChanged;
+branchFactorConfig.oninput = inputChanged;
+branchCheckBoxConfig.oninput = inputChanged;
+
 
 function saveSpecifiedWorld() {
-    if (currentWorld == null) {
+    if (currentWorld == null || worldChangedSincePreview) {
         // No preview (so generate new world):
         if (!generateWorld(false)) {
             console.error('Failed to generate a new world');
@@ -126,24 +190,24 @@ function saveSpecifiedWorld() {
         }
     }
     if (currentWorld == null) {
-        // No preview and failed to genreate new world:
+        // No preview and failed to generate new world:
         console.error('Please make a world first')
         return;
     }
-    let data = new FormData();
-    data.append("type" , wantedWorldType);
-    data.append("data" , JSON.stringify(currentWorld));
     let xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new activeXObject("Microsoft.XMLHTTP");
-    xhr.open( 'post', 'http://localhost:8000/api/create', true );
-    xhr.send(data);
+    xhr.open('post', 'http://localhost:8000/api/create', true);
+    xhr.send(JSON.stringify({
+        type: wantedWorldType,
+        world: currentWorld,
+    }));
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (this.readyState == 4) {
             if (this.status == 200) {
                 const createdInfo = JSON.parse(xhr.responseText);
                 console.log(createdInfo);
                 // Typical action to be performed when the document is ready:
-                location.href='view.html?id=' + createdInfo.id;
+                location.href = 'view.html?id=' + createdInfo.id;
             } else {
                 console.error('Backend failed:\n', xhr.responseText);
             }
