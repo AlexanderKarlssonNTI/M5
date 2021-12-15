b7cc3db1fd0dfff06149afa46401a5ad9d00f264
Erik <erik.pehrsson@elev.ga.ntig.se>
2021-12-15 17:31:40 +0100

    Mostly copied room generator in preperation for a potential world name generator which differs in content and sentence structure

--- god.js
+++ god.js
@@ -344,3 +344,14 @@ class World {
         return count;
     }
 }
+
+function WorldBaptist() {
+    const adj = ["","","","bloody","bleak","dark","clean","dirty","cozy","heavenly","hellish","beautiful","never-ending", "holy", "lovely","empty"];
+    // const plc = ["hallway","room","corridor","toilet","kitchen","basement","bedroom", "dining room","garden","chapel","dining hall","bathroom"];
+    const dsc = ["death","despair","hopelessness","healing","horror","happiness","joy","bliss", "business","love","sin","virtue","hope","corruption"];
+    let word1 = adj[Math.floor(Math.random() * adj.length)];
+    let word2 = plc[Math.floor(Math.random() * plc.length)];
+    let word3 = dsc[Math.floor(Math.random() * dsc.length)];
+    let fullName = "A "+word1+" "+word2+" of "+word3;
+    return fullName;
+}
