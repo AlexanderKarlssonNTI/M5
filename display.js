a30e614998b494cfc270d210678df900ae83e492
Erik <90757653+GeneralUser01@users.noreply.github.com>
2021-12-23 03:50:27 +0100

    Created diagonal paths

--- display.js
+++ display.js
@@ -36,31 +36,48 @@ function showWorld(world, assumeBidirectionalPaths = false) {
             const spacerAfter = document.createElement('div');
             spacerAfter.classList.add('spacer');
 
+
+            const spacerDiagonalUpperLeft = document.createElement('div');
+            spacerDiagonalUpperLeft.classList.add('spacer');
+            spacerDiagonalUpperLeft.classList.add('upper-left');
+
+            const spacerDiagonalUpperRight = document.createElement('div');
+            spacerDiagonalUpperRight.classList.add('spacer');
+            spacerDiagonalUpperRight.classList.add('upper-right');
+
+            const spacerDiagonalBottomLeft = document.createElement('div');
+            spacerDiagonalBottomLeft.classList.add('spacer');
+            spacerDiagonalBottomLeft.classList.add('bottom-left');
+
+            const spacerDiagonalBottomRight = document.createElement('div');
+            spacerDiagonalBottomRight.classList.add('spacer');
+            spacerDiagonalBottomRight.classList.add('bottom-right');
+
+
             // Create a div tag for each room:
             const roomDiv = document.createElement('div');
             roomDiv.classList.add('room');
-
-            // Add room id attribute to more spacers (allows them to be clicked and do a "room" action)
-            let addRoomIdAttribute = function () { };
 
             const roomIndex = row * world.sideLength + i;
             if (roomIndex < world.rooms.length) {
                 const room = world.rooms[roomIndex];
 
-                for (const pathElement of [spacerBefore, spacerAfter, spacerAbove, spacerBelow]) {
+                const spacers = [
+                    spacerBefore, spacerAfter, spacerAbove, spacerBelow,
+                    spacerDiagonalUpperLeft, spacerDiagonalUpperRight, spacerDiagonalBottomLeft, spacerDiagonalBottomRight,
+                ];
+                for (const pathElement of spacers) {
                     pathElement.setAttribute('data-path-from-room-id', room.ID);
                 }
 
                 // Set room id attribute to allow click events to get a room id (allows "room" actions):
-                if (assumeBidirectionalPaths) {
-                    roomDiv.setAttribute('data-room-id', room.ID);
-                } else {
-                    for (const element of [roomDiv, spacerBefore, spacerAfter, spacerAbove, spacerBelow]) {
+                roomDiv.setAttribute('data-room-id', room.ID);
+
+                if (!assumeBidirectionalPaths) {
+                    // Add room id attribute to spacers (allows them to be clicked and do a "room" action):
+                    for (const element of spacers) {
                         element.setAttribute('data-room-id', room.ID);
                     }
-                    addRoomIdAttribute = function (element) {
-                        element.setAttribute('data-room-id', room.ID);
-                    };
                 }
 
                 // Update UI with info about exits and "blocked" room:
@@ -84,8 +101,9 @@ function showWorld(world, assumeBidirectionalPaths = false) {
                             spacerAfter.setAttribute('data-path-to-room-id', targetRoom.ID);
                         }
                     }
+                    const roomAbove = totalRows <= 2 ? world.roomAboveOf(room) : world.wrappingRoomAboveOf(room);
                     {
-                        const targetRoom = totalRows <= 2 ? world.roomAboveOf(room) : world.wrappingRoomAboveOf(room);
+                        const targetRoom = roomAbove;
                         const roomAboveOf = room.hasExitTo(targetRoom);
                         roomDiv.classList.toggle('path-to-above', roomAboveOf);
                         spacerAbove.classList.toggle('has-path', roomAboveOf);
@@ -93,13 +111,53 @@ function showWorld(world, assumeBidirectionalPaths = false) {
                             spacerAbove.setAttribute('data-path-to-room-id', targetRoom.ID);
                         }
                     }
+                    const roomBelow = totalRows <= 2 ? world.roomBelowOf(room) : world.wrappingRoomBelowOf(room);
                     {
-                        const targetRoom = totalRows <= 2 ? world.roomBelowOf(room) : world.wrappingRoomBelowOf(room);
+                        const targetRoom = roomBelow;
                         const roomBelowOf = room.hasExitTo(targetRoom);
                         roomDiv.classList.toggle('path-to-below', roomBelowOf);
                         spacerBelow.classList.toggle('has-path', roomBelowOf);
                         if (targetRoom) {
                             spacerBelow.setAttribute('data-path-to-room-id', targetRoom.ID);
+                        }
+                    }
+
+                    if (world.sideLength > 2 || totalRows > 2) {
+                        {
+                            const targetRoom = world.wrappingRoomLeftOf(roomAbove);
+                            const hasPath = room.hasExitTo(targetRoom);
+                            roomDiv.classList.toggle('path-to-upper-left', hasPath);
+                            spacerDiagonalUpperLeft.classList.toggle('has-path', hasPath);
+                            if (targetRoom) {
+                                spacerDiagonalUpperLeft.setAttribute('data-path-to-room-id', targetRoom.ID);
+                            }
+                        }
+                        {
+                            const targetRoom = world.wrappingRoomRightOf(roomAbove);
+                            const hasPath = room.hasExitTo(targetRoom);
+                            roomDiv.classList.toggle('path-to-upper-right', hasPath);
+                            spacerDiagonalUpperRight.classList.toggle('has-path', hasPath);
+                            if (targetRoom) {
+                                spacerDiagonalUpperRight.setAttribute('data-path-to-room-id', targetRoom.ID);
+                            }
+                        }
+                        {
+                            const targetRoom = world.wrappingRoomLeftOf(roomBelow);
+                            const hasPath = room.hasExitTo(targetRoom);
+                            roomDiv.classList.toggle('path-to-bottom-left', hasPath);
+                            spacerDiagonalBottomLeft.classList.toggle('has-path', hasPath);
+                            if (targetRoom) {
+                                spacerDiagonalBottomLeft.setAttribute('data-path-to-room-id', targetRoom.ID);
+                            }
+                        }
+                        {
+                            const targetRoom = world.wrappingRoomRightOf(roomBelow);
+                            const hasPath = room.hasExitTo(targetRoom);
+                            roomDiv.classList.toggle('path-to-bottom-right', hasPath);
+                            spacerDiagonalBottomRight.classList.toggle('has-path', hasPath);
+                            if (targetRoom) {
+                                spacerDiagonalBottomRight.setAttribute('data-path-to-room-id', targetRoom.ID);
+                            }
                         }
                     }
                 };
@@ -113,20 +171,9 @@ function showWorld(world, assumeBidirectionalPaths = false) {
             // Spacer before room:
             if (i == 0 || !assumeBidirectionalPaths) {
                 rowDiv.appendChild(spacerBefore);
-                {
-                    // Mimic layout of normal row div in row "spacer":
-                    const spacer = document.createElement('div');
-                    spacer.classList.add('spacer');
-                    addRoomIdAttribute(spacer);
-                    rowSpacerAfter.appendChild(spacer);
-                }
-                {
-                    // Mimic layout of normal row div in row "spacer":
-                    const spacer = document.createElement('div');
-                    spacer.classList.add('spacer');
-                    addRoomIdAttribute(spacer);
-                    rowSpacerBefore.appendChild(spacer);
-                }
+                // Mimic layout of normal row div in row "spacer":
+                rowSpacerAfter.appendChild(spacerDiagonalBottomLeft);
+                rowSpacerBefore.appendChild(spacerDiagonalUpperLeft);
             }
 
             // Add room
@@ -136,20 +183,9 @@ function showWorld(world, assumeBidirectionalPaths = false) {
 
             // Spacer after room:
             rowDiv.appendChild(spacerAfter);
-            {
-                // Mimic layout of normal row div in row "spacer":
-                const spacer = document.createElement('div');
-                spacer.classList.add('spacer');
-                addRoomIdAttribute(spacer);
-                rowSpacerAfter.appendChild(spacer);
-            }
-            {
-                // Mimic layout of normal row div in row "spacer":
-                const spacer = document.createElement('div');
-                spacer.classList.add('spacer');
-                addRoomIdAttribute(spacer);
-                rowSpacerBefore.appendChild(spacer);
-            }
+            // Mimic layout of normal row div in row "spacer":
+            rowSpacerAfter.appendChild(spacerDiagonalBottomRight);
+            rowSpacerBefore.appendChild(spacerDiagonalUpperRight);
         }
 
         if (row == 0 || !assumeBidirectionalPaths) {
